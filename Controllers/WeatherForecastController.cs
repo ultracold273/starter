8b74cd704f21778bd38dfd93681d233cf00cd641
ultracold <ultracold273@users.noreply.github.com>
2023-04-18 11:57:13 +0800

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -20,12 +20,17 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet(Name = "GetAppliationGateway")]
-    public async Task<ActionResult> Post()
+public async Task<ActionResult> Post([FromBody]string? tokenFromUser)
     {
         HttpClient client = new HttpClient();
 
         var credential = new Azure.Identity.DefaultAzureCredential();
-        var token = await credential.GetTokenAsync(new Azure.Core.TokenRequestContext(new[] { "https://management.azure.com/.default" }));
+        string token;
+        if (tokenFromUser is null) {
+            token = await credential.GetTokenAsync(new Azure.Core.TokenRequestContext(new[] { "https://management.azure.com/.default" })).Token;
+        } else {
+            token = tokenFromUser;
+        }
      
         var subscriptionId = "17a663b5-f43a-4bb8-aea0-5a37a2f0cb81";
         var resourceGroupName = "jump-rg-sg";
@@ -33,7 +38,7 @@ public class WeatherForecastController : ControllerBase
         var armUri = new Uri($"https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Network/applicationGateways/{applicationGatewayName}?api-version=2022-09-01");
         using (var requestMessage = new HttpRequestMessage(HttpMethod.Get, armUri))
         {
-            requestMessage.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token.Token);
+            requestMessage.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
             var response = await client.SendAsync(requestMessage);
             
             return this.Ok(response);
